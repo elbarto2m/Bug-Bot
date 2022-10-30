53a7c00e2a5c0f2d014de2140248e433a96115ad
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-30 16:13:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = '@⁨«{>𝕩𝖳𝗈𝗌𝗁𝗂 || 𝖪𝗎𝗇𝕩<}»⁩'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global.owner = ['919609900020']
+global.premium = ['919609900020']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
